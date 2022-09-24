9a3132e42c9eeb0455f1acfd6a8b640eff2b5d91
jessica <jmillene12@gmail.com>
2022-09-24 12:13:20 -0300

    model req 2

---
+++ src/models/User.js
@@ -0,0 +1,30 @@
+const userModelSchema = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    displayName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    underscored: true,
+    timestamps:false
+  });
+  return User;
+};
+module.exports = userModelSchema;
