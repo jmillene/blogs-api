48898110026b77a60d152a6b97a68c1122d859d1
jessica <jmillene12@gmail.com>
2022-09-24 15:22:46 -0300

    req 3 em andamento

--- src/controller/auth.controler.js
+++ src/controller/auth.controler.js
@@ -2,7 +2,9 @@ const authService = require('../service/auth.service');
 
 const auth = async (req, res) => {
   const token = await authService.authenticate(req.body);
-  if (token) return res.status(200).json({ token });
+  if (token) {
+    return res.status(200).json({ token });
+}
   return res.status(400).json({ message: 'Invalid fields' });
 };
 
--- src/service/auth.service.js
+++ src/service/auth.service.js
@@ -8,9 +8,6 @@ if (!email || !password) {
 const user = await User.findOne({
   where: { email, password },
 });
-if (!user) {
-  return { type: 400, message: 'Invalid fields' };
-}
 const token = generateToken(user.DataValues);
 return { token };
 };
