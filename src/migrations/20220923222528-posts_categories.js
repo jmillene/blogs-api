1e93823ea3d325be7c6ad0309fc875404d7f0311
jessica <jmillene12@gmail.com>
2022-09-23 21:07:53 -0300

    test

--- src/migrations/20220923222528-posts_categories.js
+++ src/migrations/20220923222528-posts_categories.js
@@ -2,25 +2,31 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("posts_categories", {
-      post_id: {
+    await queryInterface.createTable('posts_categories', {
+      postId: {
         type: Sequelize.INTEGER,
         primaryKey: true,
         allowNull: false,
+        field: 'post_id',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
         references: {
-          model: "blog_posts",
-          key: "id",
-        },
-        category_id: {
-          type: Sequelize.INTEGER,
-          primaryKey: true,
-          allowNull: false,
-          references: {
-            model: "categories",
-            key: "id",
-          },
+          model: 'blog_posts',
+          key: 'id',
         },
       },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        field: 'category_id',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'categories',
+          key: 'id',
+        },
+      }
     });
   },
 
