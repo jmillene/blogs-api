5f6d4eb5c61018b28eb496fb76702974460522f9
jessica <jmillene12@gmail.com>
2022-09-25 17:51:14 -0300

    req 3 em andamento

--- src/controller/auth.controler.js
+++ src/controller/auth.controler.js
@@ -2,7 +2,13 @@ const authService = require('../service/auth.service');
 
 const auth = async (req, res) => {
   const token = await authService.authenticate(req.body);
-  if (token) return res.status(200).json({ token });
+  if (!token) {
+    console.log(token);
+    return res.status(400).json({
+      message: 'Invalid fields',
+    }); 
+    }
+    return res.status(200).json({ token });
 };
 
 module.exports = {
--- src/service/auth.service.js
+++ src/service/auth.service.js
@@ -2,16 +2,13 @@ const { User } = require('../models');
 const { generateToken } = require('../utils/JWT');
 
 const authenticate = async ({ email, password }) => {
-  const user = await User.findOne({
-    attributes: ['id', 'email', 'displayName'],
-    where: { email, password },
-  });
-  if (!user) {
-   return { type: 400, message: 'Invalid fields',
-  };
-  }
-  const token = generateToken(user);
-  return token;
+
+const user = await User.findOne({
+  attributes: ['id', 'email', 'displayName'],
+  where: { email, password },
+});
+const token = generateToken(user);
+return token;
 };
 
 module.exports = {
