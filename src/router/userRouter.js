031e7d324437bfe9618f509973fd17c497c79bec
jessica <jmillene12@gmail.com>
2022-09-26 16:09:22 -0300

    requisito 4 lint

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -4,6 +4,6 @@ const adicionaUser = require('../controller/user.controller');
 
 const router = express.Router();
  router.post('/', validaDisplayname.validaDisplayname, 
- validaDisplayname.validaEmail,validaDisplayname.validaPassword, adicionaUser.user);
+ validaDisplayname.validaEmail, validaDisplayname.validaPassword, adicionaUser.user);
 
  module.exports = router;
