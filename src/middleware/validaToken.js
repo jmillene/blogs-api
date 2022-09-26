d2e9c1dfd8f692a2b6bf159da2f2d4a8eec936c6
jessica <jmillene12@gmail.com>
2022-09-26 19:25:16 -0300

    requisito 5

--- src/middleware/validaToken.js
+++ src/middleware/validaToken.js
@@ -1,28 +1,27 @@
 const jwt = require('jsonwebtoken');
 require('dotenv');
 
-const validaToken = (req, res) => {
+const validaToken = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization) {
     return res.status(401).json({
       message: 'Token not found',
     });
   }
+  next();
 };
-const validaTokenExpirado = (req, res) => {
-  const { token } = req.body;
-  const SECRET_KEY = process.env.JWT_SECRET;
-  if (!SECRET_KEY) {
-    return res.status(401).json('teste');
+const validaTokenExpirado = async (req, res, next) => {
+  const { authorization } = req.headers;
+  try {
+    const SECRET_KEY = process.env.JWT_SECRET;
+     jwt.verify(authorization, SECRET_KEY);
+      next();
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({
+      message: 'Expired or invalid token',
+    });
   }
-  if (token) {
-    jwt.verify(token, SECRET_KEY);
-    console.log(token);
-    return res.status.json(200).json({ token });
-  }
-  return res.status(401).json({
-    message: 'Expired or invalid token',
-  });
 };
 module.exports = {
   validaToken,
