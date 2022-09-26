f9eab341fb502794065cd28b16e02b7048317493
jessica <jmillene12@gmail.com>
2022-09-26 12:40:14 -0300

    req 3

--- src/controller/auth.controler.js
+++ src/controller/auth.controler.js
@@ -2,8 +2,8 @@ const authService = require('../service/auth.service');
 
 const auth = async (req, res) => {
   const token = await authService.authenticate(req.body);
-  if (token.type) return res.status(token.type).json({ token });
-  return res.status(200).json(token);
+  if (token.type) return res.status(token.type).json(token);
+  return res.status(200).json({ token });
 };
 module.exports = {
   auth,
