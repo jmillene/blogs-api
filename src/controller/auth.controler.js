82911b3b648f8e59f81a983fa5478295d9cad875
jessica <jmillene12@gmail.com>
2022-09-26 12:10:10 -0300

    req 3

--- src/controller/auth.controler.js
+++ src/controller/auth.controler.js
@@ -2,9 +2,9 @@ const authService = require('../service/auth.service');
 
 const auth = async (req, res) => {
   const token = await authService.authenticate(req.body);
-  if (token) return res.status(200).json({ token });
+  if (token.type) return res.status(token.type).json({ token });
+  return res.status(200).json(token);
 };
-
 module.exports = {
   auth,
 };
--- src/service/auth.service.js
+++ src/service/auth.service.js
@@ -7,7 +7,6 @@ const user = await User.findOne({
   where: { email, password },
 });
 if (!user) {
-  console.log(user);
   return { type: 400, message: 'Invalid fields' };
 }
 const token = generateToken(user);
